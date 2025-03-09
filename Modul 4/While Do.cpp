0862354896a9d07deb500776d7b82885f225d7a8
Desy Permata Sari <119993025+Dee-Glit1015@users.noreply.github.com>
2025-03-09 11:41:21 +0800

    While Do

---
+++ Modul 4/While Do.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int i, nilai;
+    cout<<"masukan jumlah perulangan yang dinginkan : "; cin>>nilai;
+    do{
+        cout<<"Institut Teknologi Nasional Malang"<<endl;
+        cout<<"Falkutas Teknik Industri"<<endl;
+        i++;
+    }
+     while (i < nilai);
+}
